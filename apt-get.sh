d29b94b8188d270a86bebc844596391e2fcf56b7
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-12-13 02:47:47 +0100

    software manager install script

---
+++ apt-get.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+apt-get update
+apt-get upgrade
+
+apt-get install -y pkg-config
+apt-get install -y i3
+apt-get install -y vim-gtk
+apt-get install -y git
+apt-get install -y usb-creator-gtk
+apt-get install -y python-pip
+apt-get install -y python3-dev
+apt-get install -y curl
+apt-get install -y transmission-gtk
+apt-get install -y python-gpgme
+apt-get install -y texlive*
+apt-get install -y baobab
+apt-get install -y openjdk-7-jdk openjdk-7-jre
+apt-get install -y klavaro
+apt-get install -y libreoffice
+apt-get install -y nautilus
+apt-get install -y subversion
+apt-get install -y libreoffice
+apt-get install -y libmagickwand-dev imagemagick
+apt-get install -y sloccount
+apt-get install -y dnsutils
+apt-get install -y ngrep
+apt-get install -y tcpdump
+apt-get install -y libwww-perl
+apt-get install -y gimp ipe
