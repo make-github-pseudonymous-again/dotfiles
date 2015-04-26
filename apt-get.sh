0d88645607c44e51c864411bae32d47f036e9300
aureooms <aurelien.ooms@gmail.com>
2015-04-26 12:06:13 +0200

    elinks browser

--- apt-get.sh
+++ apt-get.sh
@@ -37,7 +37,7 @@ apt-get install -y golang
 apt-get install -y libmagickwand-dev imagemagick
 
 apt-get install -y terminator
-apt-get install -y ranger nautilus baobab ack-grep atool lynx-cur
+apt-get install -y ranger nautilus baobab ack-grep atool lynx-cur elinks
 apt-get install -y dnsutils ngrep tcpdump libwww-perl
 
 ## latex
