aeee40fa59186a08c3c36a9daf89e671ef1d1455
aureooms <aurelien.ooms@gmail.com>
2015-07-09 15:09:25 +0000

    add texlive for arch

--- pacman.sh
+++ pacman.sh
@@ -1,17 +1,43 @@
 #!/usr/bin/env sh
 
-pacman -S ranger
-pacman -S cmake
-pacman -S make
-pacman -S xorg-xrdb
-pacman -S clang
-pacman -S gcc
-pacman -S curl
-pacman -S wget
-pacman -S git
-pacman -S rsync
-pacman -S vim
-pacman -S firefox
-pacman -S npm
-pacman -S python-pip
-pacman -S python-lxml
+pacman -S --noconfirm ranger
+pacman -S --noconfirm cmake
+pacman -S --noconfirm make
+pacman -S --noconfirm xorg-xrdb
+pacman -S --noconfirm clang
+pacman -S --noconfirm gcc
+pacman -S --noconfirm curl
+pacman -S --noconfirm wget
+pacman -S --noconfirm git
+pacman -S --noconfirm rsync
+pacman -S --noconfirm vim
+pacman -S --noconfirm firefox
+pacman -S --noconfirm npm
+pacman -S --noconfirm python-pip
+pacman -S --noconfirm python-lxml
+
+# latex
+
+pacman -S --noconfirm extra/texlive-bibtexextra
+pacman -S --noconfirm extra/texlive-bin
+pacman -S --noconfirm extra/texlive-core
+pacman -S --noconfirm extra/texlive-fontsextra
+pacman -S --noconfirm extra/texlive-formatsextra
+pacman -S --noconfirm extra/texlive-games
+pacman -S --noconfirm extra/texlive-genericextra
+pacman -S --noconfirm extra/texlive-htmlxml
+pacman -S --noconfirm extra/texlive-humanities
+pacman -S --noconfirm extra/texlive-langchinese
+pacman -S --noconfirm extra/texlive-langcjk
+pacman -S --noconfirm extra/texlive-langcyrillic
+pacman -S --noconfirm extra/texlive-langextra
+pacman -S --noconfirm extra/texlive-langgreek
+pacman -S --noconfirm extra/texlive-langjapanese
+pacman -S --noconfirm extra/texlive-langkorean
+pacman -S --noconfirm extra/texlive-latexextra
+pacman -S --noconfirm extra/texlive-music
+pacman -S --noconfirm extra/texlive-pictures
+pacman -S --noconfirm extra/texlive-plainextra
+pacman -S --noconfirm extra/texlive-pstricks
+pacman -S --noconfirm extra/texlive-publishers
+pacman -S --noconfirm extra/texlive-science
