ae54ad271840ff0534e7883c47f0d7653d35682b
Aurélien Ooms <aurelien.ooms@gmail.com>
2015-11-26 12:30:31 +0100

    fix spacing

--- pacman.sh
+++ pacman.sh
@@ -64,28 +64,28 @@ pacman -S --noconfirm libreoffice-still
 
 # latex
 
-pacman -S --noconfirm extra/texlive-bibtexextra\
-extra/texlive-bin\
-extra/texlive-core\
-extra/texlive-fontsextra\
-extra/texlive-formatsextra\
-extra/texlive-games\
-extra/texlive-genericextra\
-extra/texlive-htmlxml\
-extra/texlive-humanities\
-extra/texlive-langchinese\
-extra/texlive-langcjk\
-extra/texlive-langcyrillic\
-extra/texlive-langextra\
-extra/texlive-langgreek\
-extra/texlive-langjapanese\
-extra/texlive-langkorean\
-extra/texlive-latexextra\
-extra/texlive-music\
-extra/texlive-pictures\
-extra/texlive-plainextra\
-extra/texlive-pstricks\
-extra/texlive-publishers\
+pacman -S --noconfirm extra/texlive-bibtexextra \
+extra/texlive-bin \
+extra/texlive-core \
+extra/texlive-fontsextra \
+extra/texlive-formatsextra \
+extra/texlive-games \
+extra/texlive-genericextra \
+extra/texlive-htmlxml \
+extra/texlive-humanities \
+extra/texlive-langchinese \
+extra/texlive-langcjk \
+extra/texlive-langcyrillic \
+extra/texlive-langextra \
+extra/texlive-langgreek \
+extra/texlive-langjapanese \
+extra/texlive-langkorean \
+extra/texlive-latexextra \
+extra/texlive-music \
+extra/texlive-pictures \
+extra/texlive-plainextra \
+extra/texlive-pstricks \
+extra/texlive-publishers \
 extra/texlive-science
 
 pacman -S --noconfirm xfig transfig geogebra
