1e5a9d8509f0863ce1916fdc43bd0f906804ddfa
aureooms <aurelien.ooms@gmail.com>
2015-11-27 02:22:03 +0100

    print nice message if offlineimap fails

--- .sup/hooks/before-poll.rb
+++ .sup/hooks/before-poll.rb
@@ -1,2 +1,6 @@
 say "Running offlineimap..."
-system "offlineimap", "-o", "-u", "quiet"
+system "offlineimap -o -u quiet > /dev/null 2>&1"
+if $? != 0
+  say "There was an error with offlineimap :(."
+  sleep 10
+end
