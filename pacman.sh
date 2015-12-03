b5a51e542082ab72295db4ec1b7d0c072ef0ad25
aureooms <aurelien.ooms@gmail.com>
2015-12-03 10:50:30 +0100

    xkill

--- pacman.sh
+++ pacman.sh
@@ -40,6 +40,7 @@ pacman -S --needed --noconfirm tk
 pacman -S --needed --noconfirm ack
 pacman -S --needed --noconfirm cups
 pacman -S --needed --noconfirm acpi
+pacman -S --needed --noconfirm xorg-xkill
 
 pacman -S --needed --noconfirm offlineimap
 pacman -S --needed --noconfirm msmtp
