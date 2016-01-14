d636c76fbf0c22cb964f55db7d894e857c6ebdb8
aureooms <aurelien.ooms@gmail.com>
2016-01-14 17:48:04 +0100

    useful when there is a bug

--- .sup/hooks/keybindings.rb
+++ .sup/hooks/keybindings.rb
@@ -1,3 +1,4 @@
 modes["inbox-mode"].keymap.add :archive_and_delete, "Archive and delete", :backspace
 modes["thread-index-mode"].keymap.add :toggle_archived_and_deleted, "Archive and delete thread", :backspace
 modes["thread-view-mode"].keymap.add :archive_and_delete_and_next, "Archive and delete thread, then view next", :backspace
+modes["inbox-mode"].keymap.add :remove_draft_label, "Remove draft label", "w"
