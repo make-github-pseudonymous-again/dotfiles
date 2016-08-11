f1818062858c62fcc0b4f3ce6fc3737c4f8ef940
Aurélien Ooms <aurelien.ooms@gmail.com>
2016-08-11 17:07:43 +0200

    customize shortcuts for ipe

--- .ipe/ipelets/customize.lua
+++ .ipe/ipelets/customize.lua
@@ -5,7 +5,7 @@
 shortcuts.hide_axes = "Shift+F1"
 shortcuts.reset_direction = "Shift+F2"
 shortcuts.ipelet_4_goodies = "9"  -- Rotate 90 degrees
-shortcuts.ipelet_3_goodies = "Shift+M"  -- Mirror at x-axis
+shortcuts.ipelet_3_goodies = "Shift+H"  -- Mirror at x-axis
 
 prefs.max_zoom = 200
 prefs.external_editor = "gvim -f %s"
---
+++ .ipe/ipelets/shortcuts.lua
@@ -0,0 +1,51 @@
+----------------------------------------------------------------------
+-- Ipe keyboard shortcuts
+----------------------------------------------------------------------
+--[[
+
+    This file is part of the extensible drawing editor Ipe.
+    Copyright (C) 1993-2016  Otfried Cheong
+
+    Ipe is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    As a special exception, you have permission to link Ipe with the
+    CGAL library and distribute executables, as long as you follow the
+    requirements of the Gnu General Public License in regard to all of
+    the software in the executable aside from CGAL.
+
+    Ipe is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ipe; if not, you can find it at
+    "http://www.gnu.org/copyleft/gpl.html", or write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+--]]
+
+-- on OSX, Ctrl means the Command key (for platform compatibility)
+-- if you really want the Control key, write "Control"
+-- you can also write "Command" for the Command key for clarity.
+
+shortcuts = {
+  delete = "backspace",
+  duplicate = "Shift+D",
+  edit_as_xml = "Shift+X",
+  mode_pan = "C",
+  mode_shredder = "W",
+  mode_math = "Shift+M",
+  snapvtx = "&",
+  snapctl = "é",
+  snapbd = "\"",
+  snapint = "'",
+  snapgrid = "(",
+  snapangle = "§",
+  snapauto = "è",
+  grid_visible = "à",
+  fit_page = "Alt+\\",
+}
