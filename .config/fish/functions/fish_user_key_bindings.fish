1acc4e9fc10460c9df82189942b791740da15ce8
Aurélien Ooms <aurelien.ooms@gmail.com>
2016-10-15 18:02:10 +0200

    fix

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,14 +1,16 @@
 fish_vi_key_bindings
 fzf_key_bindings
 
-### sudope ###
-set -q sudope_sequence
-or set -l sudope_sequence \cs
-test (bind | grep -q '[[:space:]]sudope$')
-or set -l do_bind
-if test (bind $sudope_sequence ^/dev/null)
-	echo "sudope: The requested sequence is already in use:" (bind $sudope_sequence | cut -d' ' -f2-)
-else if set -q do_bind
-	bind $sudope_sequence sudope
+function fish_user_key_bindings
+    ### sudope ###
+    set -q sudope_sequence
+    or set -l sudope_sequence \cs
+    test (bind | grep -q '[[:space:]]sudope$')
+    or set -l do_bind
+    if test (bind $sudope_sequence ^/dev/null)
+        echo "sudope: The requested sequence is already in use:" (bind $sudope_sequence | cut -d' ' -f2-)
+    else if set -q do_bind
+        bind $sudope_sequence sudope
+    end
+    ### sudope ###
 end
-### sudope ###
