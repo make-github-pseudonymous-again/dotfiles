ffe111637a43827c286ca5ac9d4cbdf35b2b7090
Aurélien Ooms <aurelien.ooms@gmail.com>
2016-10-18 01:01:02 +0200

    ranger archive extract

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -1,4 +1,7 @@
+import os
 from ranger.api.commands import *
+from ranger.core.loader import CommandLoader
+
 
 class Voyager(Command):
     def __init__(self, *args, prompt=None, **kws):
@@ -71,3 +74,66 @@ class fzf_select_history(Voyager):
         _fzfcmd='fasd -Rl 2> /dev/null'
         prompt="{} | {} +m".format(_fzfcmd , _fzf)
         Voyager.__init__(self, *args, **kws, prompt=prompt)
+
+class extract(Command):
+    def execute(self):
+        """ Extract selection to current directory """
+        cwd = self.fm.thisdir
+        marked_files = cwd.get_selection()
+
+        if not marked_files:
+            return
+
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
+
+        one_file = marked_files[0]
+        original_path = cwd.path
+        au_flags = ['-X', cwd.path]
+        au_flags += self.line.split()[1:]
+        au_flags += ['-e']
+
+        self.fm.copy_buffer.clear()
+        self.fm.cut_buffer = False
+        if len(marked_files) == 1:
+            descr = "extracting: " + os.path.basename(one_file.path)
+        else:
+            descr = "extracting files from: " + os.path.basename(one_file.dirname)
+        obj = CommandLoader(args=['aunpack'] + au_flags \
+                + [f.path for f in marked_files], descr=descr)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
+
+class archive(Command):
+    def execute(self):
+        """ Compress selection to current directory """
+        cwd = self.fm.thisdir
+        marked_files = cwd.get_selection()
+
+        if not marked_files:
+            return
+
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
+
+        original_path = cwd.path
+        parts = self.line.split()
+        au_flags = parts[1:]
+
+        self.fm.copy_buffer.clear()
+        self.fm.cut_buffer = False
+        descr = "compressing files in: " + os.path.basename(parts[1])
+        obj = CommandLoader(args=['apack'] + au_flags + \
+                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
+
+    def tab(self):
+        """ Complete with current folder name """
+
+        extension = ['.zip', '.tar.gz', '.rar', '.7z']
+        return ['archive ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
