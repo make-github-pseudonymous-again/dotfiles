5ee8e35cea221254799410a4e76dd9023d3041e2
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-01-19 01:05:57 +0100

    add line numbers to file preview in ranger

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -42,7 +42,8 @@ dump() { /bin/echo "$output"; }
 trim() { head -n "$maxln"; }
 
 # wraps highlight to treat exit code 141 (killed by SIGPIPE) as success
-highlight() { command highlight "$@"; test $? = 0 -o $? = 141; }
+# -n for line numbers
+highlight() { command highlight -n "$@"; test $? = 0 -o $? = 141; }
 
 case "$extension" in
     # Archive extensions:
