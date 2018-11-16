0eeae357c0792dccd043070dac216c6fd9b4f961
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-11-16 09:44:10 +0100

    :bug: fix(fish): fix sudope key bindings
    new version of fisher properly handles those outside of the custom
    config file

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,21 +2,18 @@ fish_vi_key_bindings
 fzf_key_bindings
 
 function fish_user_key_bindings
-    ### sudope ###
+    ### sudope insert mode ###
     set -q sudope_sequence
     or set -l sudope_sequence \cs
-    test (bind | grep -q '[[:space:]]sudope$')
+    test (bind | grep -q 'bind -M insert .* sudope')
     or set -l do_bind
-    if test (bind $sudope_sequence ^/dev/null)
+    if test (bind -M insert $sudope_sequence ^/dev/null)
         echo "sudope: The requested sequence is already in use:" (bind $sudope_sequence | cut -d' ' -f2-)
     else if set -q do_bind
-        bind $sudope_sequence sudope
         bind -M insert $sudope_sequence sudope
     end
-    ### sudope ###
 
-
-	# Auto-complete shortcuts
-	bind -M insert \co forward-char
-	bind -M insert \cp accept-autosuggestion
+    # Auto-complete shortcuts
+    bind -M insert \co forward-char
+    bind -M insert \cp accept-autosuggestion
 end
