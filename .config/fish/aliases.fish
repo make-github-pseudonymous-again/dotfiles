9506d0ab08a33e0be9cf0552bc96bd5bed11da3e
Aurélien Ooms <aurelien.ooms@gmail.com>
2019-06-29 19:25:48 +0200

    :art: style(.config/fish/aliases.fish): Fix alignment.

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -9,9 +9,9 @@ alias .....="cd ../../../.."
 abbr -a -- - 'cd -'
 
 # fasd aliases
-alias v='f -e vim' # quick opening files with vim
-alias m='f -e vlc' # quick opening files with vlc
-alias j='fasd_cd -d'     # cd, same functionality as j in autojump
+alias v='f -e vim'      # quick opening files with vim
+alias m='f -e vlc'      # quick opening files with vlc
+alias j='fasd_cd -d'    # cd, same functionality as j in autojump
 alias o='a -e xdg-open' # quick opening files with xdg-open
 
 # Shortcuts
