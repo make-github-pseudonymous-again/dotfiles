d9f40b9c5275a1ac2db9a0dc052d983601795284
Aurélien Ooms <aurelien.ooms@gmail.com>
2019-06-30 14:20:24 +0200

    :art: style(fish): Make some more aliases abbreviations.

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -58,29 +58,30 @@ for al in (git la)
     abbr g$al "git $al"
 end
 
-alias up="update"
-alias ss="services.show"
+abbr up "update"
+abbr ss "services.show"
 
 
 # errors from journal
-alias errors="journalctl -b -p err|less"
+abbr errors "journalctl -b -p err | less"
 
 # List all files colorized in long format
-alias l="exa -l"
+abbr l "exa -l"
 
 # List all files colorized in long format, including dot files
-alias la="exa -al"
+abbr la "exa -al"
 
 # List only directories
-alias lsd="exa -l | grep --color=never '^d'"
+abbr lsd "exa -l | grep --color=never '^d'"
 
 # Use exa instead of ls
-alias ls="exa"
+abbr ls "exa"
+
 # Colors for `ls`
 export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
 
 # Echo full path of file
-alias path="readlink -f"
+abbr path "readlink -f"
 
 # Always enable colored `grep` output
 # Note: `GREP_OPTIONS="--color=auto"` is deprecated, hence the alias usage.
@@ -95,14 +96,14 @@ command -v git > /dev/null; and alias diff="git diff --no-index --color-words"
 alias sudo='sudo '
 
 # Get week number
-alias week='date +%V'
+abbr week 'date +%V'
 
 # Stopwatch
 alias timer='echo "Timer started. Stop with Ctrl-D."; and date; and time cat; and date'
 
 # View HTTP traffic
-alias sniff="sudo ngrep -t '^(GET|POST) ' 'tcp and port 80'"
-alias httpdump="sudo tcpdump -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+abbr sniff "sudo ngrep -t '^(GET|POST) ' 'tcp and port 80'"
+abbr httpdump "sudo tcpdump -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
 # Canonical hex dump; some systems have this symlinked
 command -v hd > /dev/null; or alias hd="hexdump -C"
@@ -118,18 +119,18 @@ command -v sha1sum > /dev/null; or alias sha1sum="shasum"
 
 # One of @janmoesen’s ProTip™s
 for method in GET HEAD POST PUT DELETE TRACE OPTIONS
-	alias "$method"="lwp-request -m '$method'"
+	abbr "$method" "lwp-request -m '$method'"
 end
 
 # Make Grunt print stack traces by default
 command -v grunt > /dev/null; and alias grunt="grunt --stack"
 
 # Lock the screen (when going AFK)
-alias afk="system.LS"
+abbr afk "system.LS"
 
 # youtube-dl
-alias vdl='youtube-dl'
-alias adl='youtube-dl -f140 --'
+abbr vdl 'youtube-dl --'
+abbr adl 'youtube-dl -f140 --'
 
 # thefuck aliases
 # THIS IS WAY TOO SLOW
