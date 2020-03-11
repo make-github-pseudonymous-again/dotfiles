50d2355d696e3a8d48a380de7bcdf3ef2eac4a25
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-03-11 11:39:40 +0100

    :recycle: refactor(arxiv-downloader): Extract reusable code.

--- .bin/arxiv.py
+++ .bin/arxiv.py
@@ -1,81 +1,33 @@
 #!/usr/bin/env python3
 
 import os
-import sys
-import time
-import json
 import re
-import urllib.request
-import feedparser
-import shutil
-from collections import ChainMap
 
-log = lambda *x, **y: print(*x, **y, file=sys.stderr)
+from itertools import chain
 
-_sorted_return_codes = (
-    'done',
-    'missing config',
-    'continue',
-    'failed search query',
-    'failed download',
-)
-
-
-RC = { key: 0 if i==0 else 2**(i-1) for i, key in enumerate(_sorted_return_codes) }
-
-RC_MIRROR = { value: key for key, value in RC.items()}
-
-def _rc_decode ( code ) :
-    for key in reversed(_sorted_return_codes):
-        value = RC[key]
-        if value & code or value == code:
-            yield key
-
-def rc_decode ( code ) :
-    return list(_rc_decode(code))
+from offline import escape_filename
+from offline import sanitize
 
 DEFAULT_CACHE = os.path.expanduser("~/.cache/arxiv-downloader")
-DEFAULT_STATE = '{}/state.json'.format(DEFAULT_CACHE)
+DEFAULT_STATE = '{}/state.json'
 DEFAULT_CONFIG = os.path.expanduser("~/.config/arxiv-downloader/config.json")
 
-DEFAULT_REQUEST = 'https://export.arxiv.org/api/query?sortBy=submittedDate&sortOrder=ascending&search_query={search_query}&max_results={max_results}&start={start}'
+DEFAULT_ENDPOINT = 'https://export.arxiv.org/api/query?sortBy=submittedDate&sortOrder=ascending&search_query={search_query}&max_results={max_results}&start={start}'
+DEFAULT_FORMAT_LINKNAME = "{year:04}{month:02}{day:02} [{initials}{year:02}] {title} (arxiv:{uid})"
 
-DEFAULT_FORMAT_LINKNAME = "{year}{month}{day} [{abbr}] {title} (arxiv:{uid})"
-
-DEFAULT_THROTTLE = 3
-DEFAULT_BATCH = 1
-DEFAULT_STORAGE = '/tmp/arxiv-downloader/storage'
-DEFAULT_METADATA = '/tmp/arxiv-downloader/metadata'
-DEFAULT_INDEX = '/tmp/arxiv-downloader/index'
-DEFAULT_QUERIES = ()
-
-DEFAULT_FORMAT_SUMMARY = 'query:"{query}" total({total}) dl(metadata:{metadata}, pdf:{pdf}) state({old_state} (+{progress}) -> {new_state}) rc({rc}: {message})'
-
-DEFAULT_TIMEOUT = 60
+DEFAULT_STORAGE = '/tmp/arxiv-downloader/{type}/{uid}{ext}'
+DEFAULT_INDEX = '/tmp/arxiv-downloader/index/{tag}/{slug}{ext}'
 
 DEFAULTS = {
+    "format": DEFAULT_FORMAT_LINKNAME,
+    "storage": DEFAULT_STORAGE,
+    "index": DEFAULT_INDEX,
+
+    "endpoint": DEFAULT_ENDPOINT,
     "cache": DEFAULT_CACHE,
     "state": DEFAULT_STATE,
     "config": DEFAULT_CONFIG,
-    "request": DEFAULT_REQUEST,
-    "format": DEFAULT_FORMAT_LINKNAME,
-    "throttle": DEFAULT_THROTTLE,
-    "batch": DEFAULT_BATCH,
-    "storage": DEFAULT_STORAGE,
-    "metadata": DEFAULT_METADATA,
-    "index": DEFAULT_INDEX,
-    "queries": DEFAULT_QUERIES,
-    "timeout": DEFAULT_TIMEOUT,
-    "summary": DEFAULT_FORMAT_SUMMARY,
 }
-
-
-def escape_filename(string):
-    return string.replace('\\', '\\\\').replace('|', '\\|').replace('/', '|')
-
-
-def unescape_filename(filename):
-    return filename.replace('|', '/').replace('\\|', '|').replace('\\\\', '\\')
 
 
 def get_lastname_first_letter(name):
@@ -87,117 +39,13 @@ def get_lastname_first_letter(name):
     return lastname[0]
 
 
-def wait ( seconds ):
-    log('Sleeping for {} seconds'.format(seconds))
-    time.sleep(seconds)
-
-def sanitize ( string ):
-    return re.sub(r'\s+', ' ', string).strip()
-
-def get_arg_parser():
-
-    import argparse
-
-    parser = argparse.ArgumentParser(description='Generate a secure random PIN.')
-    parser.add_argument('--config', '-c', default=DEFAULT_CONFIG, help='config file')
-    parser.add_argument('--state', '-S', help='state file')
-    parser.add_argument('--throttle', '-t', type=int, help='query delay')
-    parser.add_argument('--batch', '-b', type=int, help='batch size')
-    parser.add_argument('--storage', '-s', help='storage path')
-    parser.add_argument('--metadata', '-m', help='metadata path')
-    parser.add_argument('--index', '-i', help='index path')
-    parser.add_argument('--queries', '-q', nargs='*', help='queries')
-    parser.add_argument('--request', '-r', help='arXiv API request')
-    parser.add_argument('--format', '-f', help='format string for filename')
-    parser.add_argument('--timeout', '-T', help='timeout for requests')
-    parser.add_argument('--summary', help='format string for summary')
-
-    return parser
-
-def get_slug ( format , title=None, abbr=None, **kwargs ) :
-
-    # does not work if title appears twice in format
-
-    max_filename_length = 255 - 5 # Take into account .json suffix
-    # max_abbr_length = 3
-
-    # abbr_truncated = (abbr[:max_abbr_length] + '+') if len(abbr) > max_abbr_length else abbr
-
-    abbr_truncated = abbr
-
-    slug = format.format(title=title, abbr=abbr_truncated, **kwargs)
-
-    slug_escaped = escape_filename(slug)
-
-    extra = len(slug_escaped) - max_filename_length
-
-    if extra > 0:
-
-        title_truncated = title[:-(2*extra+2)] + '..' # * 2 in case of escaped characters
-
-        slug = format.format(title=title_truncated, abbr=abbr_truncated, **kwargs)
-
-        slug_escaped = escape_filename(slug)
-
-    return slug_escaped
-
-
-def get_params ( kwargs ) :
-
-    kwargs = { key: value for key, value in kwargs.items() if value is not None }
-
-    config = os.path.expanduser(kwargs['config'])
-    log('config file: {}'.format(config))
-
-    try:
-        with open(config) as fp:
-            _config = json.load(fp)
-    except:
-        if config == DEFAULT_CONFIG:
-            pass
-        else:
-            log("Could not open config file ({})".format(config))
-            sys.exit(RC['missing config'])
-
-    params = ChainMap(kwargs, _config, DEFAULTS)
-
-    log('params details')
-    json.dump(vars(params), sys.stderr, indent=2)
-    log()
-    log('params')
-    json.dump(dict(**params), sys.stderr, indent=2)
-    log()
-
-    return params
-
-def get_state ( params ) :
-
-    state = os.path.expanduser(params['state'])
-    log('state file: {}'.format(state))
-
-    try:
-        with open(state) as fp:
-            _state = json.load(fp)
-    except:
-        _state = {}
-
-
-    log('state')
-    json.dump(_state, sys.stderr, indent=2)
-    log()
-
-    return _state
-
 def extract_tags ( term ) :
     matches = re.findall(r"[\w.]+", term)
     return filter(is_tag, map(sanitize_tag, matches))
 
 def sanitize_tag ( string ) :
-
     string = string.lower()
-
     string = string.strip(' .')
-
     return string
 
 NOT_A_TAG = {
@@ -206,13 +54,62 @@ NOT_A_TAG = {
 }
 
 def is_tag ( string ) :
-
-    if string in NOT_A_TAG: return False
-
-    return True
+    return string not in NOT_A_TAG
 
 
-def download ( url , filename , **kwargs ) :
-    with urllib.request.urlopen(url, **kwargs) as response:
-        with open(filename, 'wb') as fp:
-            shutil.copyfileobj(response, fp)
+get_uid = lambda m: m['id'].split('/abs/')[-1]
+
+def get_resources ( m ) :
+
+    for link in filter(lambda x: x.get('type', None) == 'application/pdf', m.get('links', [])):
+
+        yield {
+            "type": "storage",
+            "ext" : ".pdf",
+            "url" : link['href'] ,
+            "content_type": "application/pdf",
+        }
+
+        break
+
+def compile_metadata ( title=None, authors=None, **entry ) :
+
+    title = sanitize(title)
+    authors = list(map(lambda x: sanitize(x['name']), authors))
+    published = entry['published']
+
+    year, month, day, *_ = entry['published_parsed']
+
+    initials = ''.join(
+        sorted(map(get_lastname_first_letter, authors)))
+
+    # max_initials_length = 3
+    # initials_truncated = (initials[:max_initials_length] + '+') if len(initials) > max_initials_length else initials
+
+    return dict(
+        **entry,
+        title=title,
+        authors=authors,
+        year=year,
+        month=month,
+        day=day,
+        initials=initials,
+    )
+
+
+def generate_tags ( entry ) :
+
+    authors = list(map(lambda x: sanitize(x['name']), entry['authors']))
+    tags = list(chain(*map(lambda x: extract_tags(x['term']), entry['tags'])))
+
+    for resource in get_resources( entry ) :
+
+        yield resource, "all"
+
+        for author in authors:
+            yield resource, "author/{}".format(escape_filename(author))
+
+        for tag in tags:
+            yield resource, "tag/{}".format(escape_filename(tag))
+
+
---
+++ .bin/offline.py
@@ -0,0 +1,504 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import time
+import json
+import re
+import urllib.request
+import feedparser
+import shutil
+
+from itertools import islice
+
+from collections import ChainMap
+from collections import Counter
+from collections import defaultdict
+
+from copy import copy
+
+log = lambda *x, **y: print(*x, **y, file=sys.stderr)
+
+delimit = lambda c: log(c*79)
+
+def logjson ( x , fp = sys.stderr , indent=2 ) :
+    json.dump(x, fp, indent=indent)
+    log()
+
+def dumpjson ( x , fp = sys.stdout, separators=(',', ':') ) :
+    # https://docs.python.org/3/library/json.html?highlight=json#json.dump
+    json.dump(x, fp, separators=separators)
+
+
+_sorted_return_codes = (
+    'done',
+    'missing config',
+    'mangled config',
+    'continue',
+    'failed search query',
+    'failed download',
+)
+
+
+RC = { key: 0 if i==0 else 2**(i-1) for i, key in enumerate(_sorted_return_codes) }
+
+RC_MIRROR = { value: key for key, value in RC.items()}
+
+def _rc_decode ( code ) :
+    for key in reversed(_sorted_return_codes):
+        value = RC[key]
+        if value & code or value == code:
+            yield key
+
+def rc_decode ( code ) :
+    return list(_rc_decode(code))
+
+# TECHNICAL
+DEFAULT_THROTTLE = 3
+DEFAULT_BATCH = 1
+DEFAULT_TIMEOUT = 60
+DEFAULT_FORMAT_SUMMARY = 'query:"{query}" total({total}) dl({counts}) state({old_state} (+{progress}) -> {new_state}) rc({rc}: {message})'
+
+# SAFE DEFAULTS FOR TESTING
+DEFAULT_CHECK = False
+DEFAULT_STORAGE = '/tmp/offline/{type}/{uid}{ext}'
+DEFAULT_METADATA_TYPE = 'metadata'
+DEFAULT_METADATA_EXTENSION = '.json'
+DEFAULT_INDEX = '/tmp/offline/index/{tag}/{slug}{ext}'
+DEFAULT_FORMAT_LINKNAME = "{uid}"
+DEFAULT_QUERIES = ()
+
+
+DEFAULTS = {
+
+    "throttle": DEFAULT_THROTTLE,
+    "batch": DEFAULT_BATCH,
+    "timeout": DEFAULT_TIMEOUT,
+    "summary": DEFAULT_FORMAT_SUMMARY,
+
+    "check": DEFAULT_CHECK,
+    "storage": DEFAULT_STORAGE,
+    "metadata_type": DEFAULT_METADATA_TYPE,
+    "metadata_ext": DEFAULT_METADATA_EXTENSION,
+    "index": DEFAULT_INDEX,
+    "format": DEFAULT_FORMAT_LINKNAME,
+    "queries": DEFAULT_QUERIES,
+}
+
+
+def escape_filename(string):
+    return string.replace('\\', '\\\\').replace('|', '\\|').replace('/', '|')
+
+
+def unescape_filename(filename):
+    return filename.replace('|', '/').replace('\\|', '|').replace('\\\\', '\\')
+
+def wait ( seconds ):
+    log('Sleeping for {} seconds'.format(seconds))
+    time.sleep(seconds)
+
+def sanitize ( string ):
+    return re.sub(r'\s+', ' ', string).strip()
+
+def get_arg_parser(default_config=None, description = 'Feed downloader.'):
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description=description)
+
+    # These are purely technical and have sensible defaults.
+    parser.add_argument('--throttle', '-t', type=int, help='query delay')
+    parser.add_argument('--timeout', '-T', help='timeout for requests')
+    parser.add_argument('--batch', '-b', type=int, help='batch size')
+    parser.add_argument('--summary', help='format string for summary')
+
+    # Those do not have sensible defaults but one can run the program without.
+    parser.add_argument('--config', '-c', default=default_config, help='config file')
+    parser.add_argument('--state', '-S', help='state file')
+
+    # These can have default for testing purposes.
+    parser.add_argument('--check', '-C', help='check that downloaded files have correct size')
+    parser.add_argument('--storage', '-s', help='storage path format')
+    parser.add_argument('--index', '-i', help='index path')
+    parser.add_argument('--format', '-f', help='format string for filename')
+    parser.add_argument('--queries', '-q', nargs='*', help='queries')
+    parser.add_argument('--metadata-type', help='resource type for metadata')
+    parser.add_argument('--metadata-ext', help='file extension for metadata')
+
+    # These do not have defaults and it does not make sense for the library to
+    # set a default.
+    parser.add_argument('--endpoint', '-r', help='endpoint')
+
+    return parser
+
+def get_slug ( format , title=None, **kwargs ) :
+
+    # does not work if title appears twice in format
+
+    max_filename_length = 255 - 5 # Take into account .json suffix
+
+    slug = format.format(title=title, **kwargs)
+
+    slug_escaped = escape_filename(slug)
+
+    extra = len(slug_escaped) - max_filename_length
+
+    if extra > 0:
+
+        title_truncated = title[:-(2*extra+2)] + '..' # * 2 in case of escaped characters
+
+        slug = format.format(title=title_truncated, **kwargs)
+
+        slug_escaped = escape_filename(slug)
+
+    return slug_escaped
+
+
+def get_params ( kwargs, *defaults ) :
+
+    kwargs = { key: value for key, value in kwargs.items() if value is not None }
+
+    kwargs2 = ChainMap(kwargs, *defaults)
+
+    if kwargs2['config'] is None:
+        log("Warning: working without a config file!")
+        _config = {}
+    else:
+        config = os.path.expanduser(kwargs2['config'])
+        log('config file: {}'.format(config))
+
+        try:
+            with open(config) as fp:
+                _config = json.load(fp)
+        except json.decoder.JSONDecodeError as e:
+            log("Could not load config file ({})".format(config))
+            log(e)
+            sys.exit(RC['mangled config'])
+        except:
+            if config == ChainMap(*defaults, DEFAULTS).get('config', None):
+                log("Warning: could not open default config file!")
+                _config = {}
+            else:
+                log("Could not open config file ({})".format(config))
+                sys.exit(RC['missing config'])
+
+    params = ChainMap(kwargs, _config, *defaults, DEFAULTS)
+
+    log('params details')
+    logjson(vars(params))
+    log('params')
+    logjson(dict(**params))
+
+    return params
+
+def get_state_path ( params ) :
+    return get_path_from_format_or_execute(params['state'], params['cache'])
+
+def get_state ( params ) :
+
+    _state = {}
+
+    if 'state' in params:
+        state = get_state_path(params)
+        log('state file: {}'.format(state))
+
+        try:
+            with open(state) as fp:
+                _state = json.load(fp)
+        except:
+            log('State file is configured but could not load its contents!')
+
+    else:
+        log('No state path provided. Will proceed without it.')
+
+
+    log('state')
+    logjson(_state)
+
+    return Counter(_state)
+
+def dump_state ( params , _state):
+    if 'state' in params:
+        state = get_state_path(params)
+        os.makedirs(os.path.dirname(state), exist_ok=True)
+        with open(state, 'w') as fp:
+            dumpjson(_state, fp)
+    else:
+        log('Not saving state because no state path given. Here it is anyway!')
+        dumpjson(_state)
+
+def urlopen ( url , **kwargs) :
+    request = urllib.request.Request(url, headers={'User-Agent': 'Mozilla'})
+    return urllib.request.urlopen(request, **kwargs)
+
+class ContentTypeMismatchException(Exception):
+    def __init__ ( self , *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+def validate_content_length ( url , path , **kwargs ) :
+
+    try:
+        size = os.path.getsize(path)
+        log("Validating content-length for {}".format(url))
+        with urlopen(url, **kwargs) as response:
+            content_length = int(response.info().get('Content-Length'))
+
+        if size == content_length: return True
+
+        log("Content length mismatch: expected {} got {}".format(content_length, size))
+
+    except Exception as e:
+        log("Could not validate content-length.")
+        log(e)
+
+    return False
+
+
+def download ( url , path , expected_content_type = None , **kwargs ) :
+
+    log("Checking headers of {}".format(url))
+    with urlopen(url, **kwargs) as response:
+
+        if expected_content_type is not None:
+            content_type = response.info().get_content_type()
+            if content_type != expected_content_type:
+                msg = '{}: Received {} while expecting {}'.format(url, content_type, expected_content_type)
+                raise ContentTypeMismatchException(msg)
+
+        log("Downloading {} to {}.".format(url, path))
+        with open(path, 'wb') as fp:
+            # https://github.com/python/cpython/blob/7b3ab5921fa25ed8b97b6296f97c5c78aacf5447/Lib/shutil.py#L194-L205
+            # implement progress bar?
+            shutil.copyfileobj(response, fp)
+
+def format_or_execute ( string_or_function, *args, **kwargs ) :
+
+    if isinstance(string_or_function, str):
+        fn = string_or_function.format
+    else:
+        fn = string_or_function
+
+    return fn(*args, **kwargs)
+
+def get_path_from_format_or_execute ( *args, **kwargs ) :
+    path = format_or_execute(*args, **kwargs)
+    return os.path.expanduser(path)
+
+def download_once (
+        _state = None , throttle = None , batch = None ,
+        storage = None , metadata_type = None, metadata_ext = None, queries = None ,
+        endpoint = None , timeout = None, summary = None,
+        get_uid = None, get_resources = None , check = None , reverse_entries = False, **kwargs ) :
+
+    delimit('=')
+
+    _old_state = copy(_state)
+    rc = defaultdict(lambda: RC['done'])
+    count = defaultdict(Counter)
+    total = Counter()
+
+    for search_query in queries:
+        log("search_query", search_query)
+        start = _state.setdefault(search_query, 0)
+
+        query_url = endpoint.format(search_query=search_query, start=start, max_results=batch)
+        log(query_url)
+
+        wait(throttle)
+        try:
+            response = urlopen(query_url, timeout=timeout)
+        except Exception as e:
+            log('Failed to query', query_url)
+            log(e)
+            rc[search_query] = RC['failed search query']
+            break
+
+        document = feedparser.parse(response)
+        feed = document.feed
+
+        opensearch_startindex = int(feed.get('opensearch_startindex', 0))
+        total[search_query] = int(feed.get('opensearch_totalresults', -1))
+
+        increment_state = 1
+
+        _start = start-opensearch_startindex
+        _stop = _start + batch
+
+        entries = document.entries
+        if reverse_entries: entries = reversed(entries)
+        entries = islice(entries, _start, _stop)
+
+        for entry in entries:
+
+            delimit('+')
+
+            uid = get_uid(entry)
+            uid_escaped = escape_filename(uid)
+
+            logmetadata(uid, entry)
+
+            metadata_path = get_path_from_format_or_execute(storage, type=metadata_type, uid=uid_escaped, ext=metadata_ext)
+            metadata_dir = os.path.dirname(metadata_path)
+
+            for i, resource in enumerate(get_resources(entry), 1):
+
+                resource_type = resource['type']
+                resource_url = resource['url']
+                resource_ext = resource['ext']
+                resource_content_type = resource.get('content_type', None)
+                resource_path = get_path_from_format_or_execute(storage, type=resource_type, uid=uid_escaped, ext=resource_ext)
+                resource_dir = os.path.dirname(resource_path)
+
+                log('Resource #{}: {}'.format(i, resource_url))
+
+                try:
+                    exists = os.path.getsize(resource_path) > 0
+                except:
+                    exists = False
+
+                if exists and (not check or validate_content_length(resource_url, resource_path, timeout=timeout)):
+                    log("{} already exists, skipping".format(resource_path))
+                else:
+                    os.makedirs(resource_dir, exist_ok=True)
+
+                    wait(throttle)
+
+                    try:
+                        download(resource_url, resource_path, expected_content_type=resource_content_type, timeout=timeout)
+                        count[search_query][resource_type] += 1
+                    except Exception as e:
+                        log("Failed to download", resource_url)
+                        log(e)
+                        rc[search_query] = RC['failed download']
+                        try:
+                            log("Cleaning up...")
+                            if os.path.exists(resource_path):
+                                log("Removing", resource_path)
+                                os.remove(resource_path)
+                                log("Success :)")
+                            else:
+                                log("Nothing to do!")
+                        except Exception as e:
+                            log("Clean up failed :(")
+                            log(e)
+
+                        increment_state = 0
+
+                        if not isinstance(e, ContentTypeMismatchException):
+                            break
+
+                os.makedirs(metadata_dir, exist_ok=True)
+                with open(metadata_path, 'w') as fp:
+                    dumpjson(entry, fp)
+                    count[search_query]['metadata'] += 1
+
+                _state[search_query] += increment_state
+
+        delimit('+')
+
+
+    dump_state(kwargs, _state)
+
+    for search_query in queries:
+        incomplete_single_query = opensearch_startindex == 0 and _state[search_query] < len(document.entries)
+        if (total[search_query] < 0 and incomplete_single_query) or _state[search_query] < total[search_query]:
+            rc[search_query] = max(rc[search_query], RC['continue'])
+
+    delimit('=')
+
+    print_summary( summary , queries , _old_state , _state , count , rc , total )
+
+    delimit('=')
+
+    return max(rc.values())
+
+
+def print_summary ( summary , queries, old_state, new_state, count, rc , total) :
+
+    for search_query in queries:
+
+        _old_state = old_state[search_query]
+        _new_state = new_state[search_query]
+        _progress = _new_state - _old_state
+
+        counts = ', '.join(map(lambda x: '{}: {}'.format(*x), sorted(count[search_query].items()))) or '-'
+        message = ', '.join(rc_decode(rc[search_query]))
+
+        summary_options = dict(
+            query = search_query,
+            counts = counts,
+            old_state = _old_state,
+            new_state = _new_state,
+            progress = _progress,
+            message = message,
+            rc = rc[search_query],
+            total = total[search_query],
+        )
+
+        summary_string = format_or_execute(summary, **summary_options)
+
+        log(summary_string)
+
+def logmetadata ( uid , entry ) :
+
+    title = sanitize(entry['title'])
+    authors_names = list(map(lambda x: sanitize(x['name']), entry['authors']))
+
+    log(' + uid: {}'.format(uid))
+    log(' + permalink: {}'.format(entry['id']))
+    log(' + Published: {}'.format(entry['published']))
+    log(' + Title:  {}'.format(title))
+    log(' + Authors:  {}'.format(', '.join(authors_names)))
+    # log('Summary:  {}'.format(entry['summary']))
+
+
+get_uid_from_filename = lambda x: unescape_filename(os.path.splitext(x)[0])
+
+
+def index_once ( uids , _state = None , state = None ,
+        storage = None , metadata_type = None , metadata_ext = None , index = None ,
+        format = None, compile_metadata = None, generate_tags = None, **kwargs ) :
+
+        delimit('=')
+
+        for uid in uids:
+
+            log('indexing', uid)
+
+            uid_escaped = escape_filename(uid)
+            metadata_path = get_path_from_format_or_execute(storage, type=metadata_type, uid=uid_escaped, ext=metadata_ext)
+
+            with open(metadata_path) as fd:
+                _metadata = json.load(fd)
+
+            logmetadata(uid, _metadata)
+
+            info = compile_metadata(**_metadata)
+
+            slug = get_slug(format, uid=uid, **info)
+
+            for resource, tag in generate_tags(_metadata):
+
+                resource_type = resource['type']
+                resource_ext = resource['ext']
+                resource_path = get_path_from_format_or_execute(storage, type=resource_type, uid=uid_escaped, ext=resource_ext)
+
+                link_path = get_path_from_format_or_execute(index, tag=tag, slug=slug, ext=resource_ext)
+                link_dir = os.path.dirname(link_path)
+                os.makedirs(link_dir, exist_ok=True)
+                try:
+                    os.symlink(resource_path, link_path)
+                except FileExistsError as e:
+                    pass
+
+        delimit('=')
+
+def index_init ( storage = None , metadata_type = None , **kwargs ) :
+
+    metadata_dir = get_path_from_format_or_execute(storage, type=metadata_type, uid='', ext='')
+
+    ignore_syncthing_conflicts = lambda x: 'sync-conflict' not in x
+
+    uids = filter(ignore_syncthing_conflicts, map(get_uid_from_filename, os.listdir(metadata_dir)))
+
+    return index_once( uids , storage = storage , metadata_type = metadata_type, **kwargs )
