d781dcd2c520fd8962bbf94e87f9a753b0bc18aa
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-07-27 22:43:32 +0200

    :recycle: refactor(calendar): Simplify even further.

--- .bin/events.py
+++ .bin/events.py
@@ -128,3 +128,150 @@ def _dropalarms(string):
 
 def dropalarms(string):
     return '\n'.join(_dropalarms(string))
+
+
+def main ( now , events ) :
+
+    _fresh = list(filter(lambda x: x.end >= now, events))
+    _future = filter(lambda x: x.begin >= now, _fresh)
+    _happening = filter(lambda x: x.begin < now, _fresh)
+    _current = min(_happening, key=lambda x: x.end, default=None)
+    _next = min(_future, key=lambda x: x.begin, default=None)
+
+    if _current is None:
+
+        if _next is None:
+
+            raise ValueError('no main event')
+
+        else:
+
+            return _next
+
+    else:
+
+        return _current if _next is None or _next.begin >= _current.end else _next
+
+
+def event_to_i3_status_object ( now , event ) :
+
+    name = event.name
+    location = event.location
+    short_location = location.split(',')[0] if location else None
+    begin = event.begin.to('local')
+    end = event.end.to('local')
+
+    # event format
+    ef = '{name}'
+    if location and location not in name:
+        ef += ' ( {location})'
+
+    if begin < now:
+        ef += ' - (started {hbegin}, ends {hend})'
+    else:
+        ef += ' - {range} ({hbegin})'
+
+    full_text = ef.format(
+        range = _range( now , begin , end ) ,
+        hbegin = begin.humanize(),
+        hend = end.humanize( ) ,
+        name = name,
+        location = location
+    )
+
+    full_text = full_text.replace('\r', '')
+    full_text = full_text.replace('\n', ' ')
+
+    short_text = ef.format(
+        range = _range( now , begin , end ) ,
+        hbegin = begin.humanize(),
+        hend = end.humanize( ) ,
+        name = (name[:47] + '..') if len(name) > 49 else name,
+        location = short_location
+    )
+
+    short_text = short_text.replace('\r', '')
+    short_text = short_text.replace('\n', ' ')
+
+    return {
+        "name":"calendar",
+        "color":"#A7C5BD",
+        "full_text":" {}".format(full_text),
+        "short_text":" {}".format(short_text)
+    }
+
+
+# FORMAT TIME
+
+def _humanize(time, ref):
+
+    x = time.humanize(ref)
+
+    if x == "in seconds":
+
+        return "in {} seconds".format((time - ref).seconds)
+
+    if x == "just now":
+
+        return "now"
+
+    return x
+
+
+def _duration(A, B):
+
+    x = _humanize(B, A)
+
+    if x == 'now':
+
+        return '{} seconds'.format((B - A).seconds)
+
+    if x == 'in a minute':
+
+        return '1 minute'
+
+    return x[3:]
+
+
+def _shortduration(A, B):
+
+    return _shorten(_duration(A, B))
+
+
+def _shorthumanize(time, ref):
+
+    return _shorten(_humanize(time, ref))
+
+def _tinyduration(A, B):
+
+    return _tinier(_duration(A, B))
+
+
+def _tinyhumanize(time, ref):
+
+    return _tinier(_humanize(time, ref))
+
+
+def _shorten(x):
+
+    if 'minutes' in x or 'seconds' in x:
+
+        return x[:-4]
+
+    if 'minute' in x:
+
+        return x[:-3]
+
+    return x
+
+def _tinier(x):
+
+    if 'minutes' in x or 'seconds' in x:
+
+        return x[:-6]
+
+    if 'minute' in x:
+
+        return x[:-5]
+
+    return x
