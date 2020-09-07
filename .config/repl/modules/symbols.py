9b13f922e771a08fd59ba408baf500cf8453b7bd
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-09-07 16:33:16 +0200

    :sparkles: feat(repl): Define more symbols.

--- .config/repl/modules/symbols.py
+++ .config/repl/modules/symbols.py
@@ -1,6 +1,14 @@
 from sympy import symbols
+from sympy import Symbol
 from sympy import Function
+from sympy import beta, gamma
 
 x, y, z, t = symbols('x y z t')
 k, m, n = symbols('k m n', integer=True)
 f, g, h = symbols('f g h', cls=Function)
+Beta = beta
+Gamma = gamma
+alpha = Symbol('alpha')
+beta = Symbol('beta')
+gamma = Symbol('gamma')
+K = Symbol('K')
