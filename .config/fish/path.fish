8481cbb999f84b7dee2b9a946998acf955aeab76
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-07-27 12:16:23 +0200

    :adhesive_bandage: fix(`fish`): Handle missing `ruby` executable.

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -10,9 +10,11 @@ if test -d $HOME/.meteor
 end
 
 # ruby gems
-set -gx RUBY_PATH (ruby -e 'print Gem.user_dir')/bin
-if test -d $RUBY_PATH
-  set -gx PATH $RUBY_PATH $PATH
+if command -q ruby
+  set -gx RUBY_PATH (ruby -e 'print Gem.user_dir')/bin
+  if test -d $RUBY_PATH
+    set -gx PATH $RUBY_PATH $PATH
+  end
 end
 
 # node path
